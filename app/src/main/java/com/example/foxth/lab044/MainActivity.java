95524d4b70568b37de1f29d3aaff09844357bf63
Adam Perlinski <foxtheowner@gmail.com>
2018-05-23 02:07:11 +0200

    Added MYSQLITE to an app.

--- app/src/main/java/com/example/foxth/lab044/MainActivity.java
+++ app/src/main/java/com/example/foxth/lab044/MainActivity.java
@@ -1,64 +1,99 @@
 package com.example.foxth.lab044;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+    private MySQLite db = new MySQLite(this);
+
+    String[] values = new String[] { "Pies", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
+            "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] values = new String[] { "Pies",
-                "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
-                "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
-        this.adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,this.target);
-        ListView listview = (ListView) findViewById(
-                R.id.Lista );
+
+        this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(), new String[] {"_id", "gatunek"}, new int[] {android.R.id.text1, android.R.id.text2}, SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+        ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 2);
+            }
+        });
+
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override public boolean
+            onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+
+                db.usun(String.valueOf(zwierz.getID()));
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+
+                return true; }
+        });
     }
 
-    @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode==1 && resultCode==RESULT_OK)
-        {
-            Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
-    public void nowyWpis(MenuItem mi)
-    {
-        Intent intencja = new Intent(this,
-                DodajWpis.class);
+    public void nowyWpis(MenuItem mi) {
+        Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+
+        if(requestCode==2 && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.aktualizuj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+
+    }
 
 }
